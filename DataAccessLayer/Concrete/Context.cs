e691cd44c7f8862a5dd12ddcf0a9c00f3114018f
Baran <barandasdemir.bd@gmail.com>
2025-07-08 14:13:13 +0300

    identity

--- DataAccessLayer/Concrete/Context.cs
+++ DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,6 @@
-﻿using EntityLayer.Concrete;
+﻿using EntityLayer;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace DataAccessLayer.Concrete
 {
-    public class Context : DbContext
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
---
+++ EntityLayer/Concrete/AppUser.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class AppUser:IdentityUser<int> //buradaki int ile IdentityUser<int> sınıfındaki Id tipi eşleşiyor böylelikle primary key olarak int kullanmış oluyoruz ve atama yapmış oluyoruz
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+    }
+}
